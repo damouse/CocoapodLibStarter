77a8455eac5b27bfc009a21a4a5c65d83c98d808
damouse <damouse@damouses-Mac.local>
2016-05-24 16:06:19 -0500

    initial commit for starter repository

---
+++ Silvery/Model.swift
@@ -0,0 +1,8 @@
+
+// Some sample starter file. You should replace this!
+
+import Foundation
+
+public func add(a: Int, _ b: Int) -> Int {
+    return a + b
+}
---
+++ Starter.playground/Contents.swift
@@ -0,0 +1,11 @@
+/*:
+# Silvery
+Try out Silvery here!
+*/
+import Foundation
+import Starter
+
+
+let a = add(1, 2)
+
+print(a)
